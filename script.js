f05a94f90303a52f765d15c419e572c49c8795bd
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-07-08 11:29:39 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -10,4 +10,40 @@ hamburger.addEventListener("click", ()=>{
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", ()=> {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+}));
+
+
+
+// Scroll reveal
+
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
